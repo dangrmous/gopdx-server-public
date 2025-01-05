6c19dc1709207bdfec3b2ffd7fa0fbd5107297cf
gunger <geoffunger@gmail.com>
2025-01-04 17:17:44 -0800

    Removed old configs

--- gopdxServerConfig.js
+++
@@ -1,9 +0,0 @@
-var configs =
-{
-    appVersion: '0.1.15',
-    serverVersion: '0.1.15',
-    trimetAppID:'YOUR-TRIMET-APP-KEY',
-    mapQuestAPIKey: 'YOUR-MAPQUEST-APP-KEY'
-}
-
-module.exports = configs;
